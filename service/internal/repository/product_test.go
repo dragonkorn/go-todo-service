cfc53c07ed17b86fbdc86833ac5de9888b17283b
dragonkorn <dragonballpreecha@gmail.com>
2021-11-23 00:14:08 +0700

    add example test file

---
+++ service/internal/repository/product_test.go
@@ -0,0 +1,15 @@
+package repository_test
+
+import "testing"
+
+func TestCreateProduct(t *testing.T) {
+	/* arrange */
+	// given := ...
+	// want := ...
+
+	/* act */
+	// get := targetFunction()
+
+	/* assert */
+	// check is want equal to get ?
+}
